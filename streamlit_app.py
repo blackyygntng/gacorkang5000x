d34217a378d3626621b46c28ca463577b1cefed0
blackyygntng <adityaafernandoo@gmail.com>
2025-04-24 13:20:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("gacorkang5000x")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "belajar menjadi bos gacor
 )
