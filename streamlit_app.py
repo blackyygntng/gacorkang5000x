269428fc166360ddae618642e670dedaab8bc4c0
blackyygntng <adityaafernandoo@gmail.com>
2025-05-15 14:38:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("selamat datang di website gacorkang5000x")
-st.image("image.jpg")
+st.image("image.jpg",width=200)
 st.write("\n\")
 st.subheader("Aku 'Suka' Rofi")
 
