3379c46ff920061413523be4f7afd3bf1d9bafab
blackyygntng <adityaafernandoo@gmail.com>
2025-05-15 14:53:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Selamat datang di website informatika")
-st.image("views/image.jpg", width=200)
+st.image("views/image.jpg",width=200)
 st.write("\n")
 st.subheader("Muhammad 'Mister' Ahsan")
 st.write(
